1fc4acbe2c6c31a1ee957c6a5ce1e0bea13e2799
GlassToeStudio <GlassToeStudio@gmail.com>
2019-02-28 06:11:27 -0600

    Add BaseViewModels for MVVM

---
+++ GTS-SDK-Manager/SDKTools/PakcageItemStructure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTS_SDK_Manager.SDKTools
+{
+    public class PakcageItemStructure
+    {
+        public async Task<string[]> GetAllData()
+        {
+            return await Task<string[]>.Run(async () => {
+                return await PackageStructure.RunSDKManagerListVerbose(null);
+            });
+
+        } // Get AllData from another class.
+
+        public List<PackageItem> PackageItems { get; set; } = new List<PackageItem>();
+    }
+}
