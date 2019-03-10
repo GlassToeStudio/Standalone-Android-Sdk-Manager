b2bd61460448f2e243abc94884b05b303521e008
GlassToeStudio <GlassToeStudio@gmail.com>
2019-03-10 11:20:54 -0500

    Add color change effect for status indicator.
    Color will change depending on progress of install/update/delete tasks. Current only yellow while downloading and green while unzipping.

---
+++ SdkManager.UI/ValueConverters/StringToColorConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using SdkManager.Core;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace SdkManager.UI
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class StringToColorConverter : IValueConverter
+    {
+        public static StringToColorConverter Instance = new StringToColorConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string c = "#BBBBBB";
+            if (value != null)
+            {
+                var status = (string)value;
+
+                /* could go on 
+                 * [                                       ] // Red
+                 * [=                                      ] 
+                 * [==                                     ] // ...
+                 * [===                                    ]
+                 * [=====                                  ] // Yellow
+                 * [========                               ]
+                 * [===========                            ] // ...
+                 * {=====================                  ]
+                 * [=======================================] // Green
+                 * 
+                 * For example
+                 */
+
+                if (status.Contains("Downloading"))
+                {
+                    c = "Yellow";
+                }
+                if (status.Contains("Unzipping"))
+                {
+                    c = "Green";
+                }
+            }
+            return c;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
