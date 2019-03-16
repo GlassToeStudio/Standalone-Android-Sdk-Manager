302068a342e8f5df820f725b6e680e4c6543b67c
GlassToeStudio <GlassToeStudio@gmail.com>
2019-03-16 07:06:50 -0500

    Udpate some doc comments

--- SdkManager.Core/SDKManager/SdkManagerBat.cs
+++ SdkManager.Core/SDKManager/SdkManagerBat.cs
@@ -37,7 +37,7 @@ namespace SdkManager.Core
 
         /// <summary>
         /// The path to sdkmanager.bat
-        /// </summary> TODO: Intentional Typeo!!!!!!!!!!!!!!!!!!!!!!
+        /// </summary>
         public static string PathName { get => _pathName;
             set
             {
@@ -56,12 +56,12 @@ namespace SdkManager.Core
         #region Events/Actions
 
         /// <summary>
-        /// Listen for this event to get the output from the hidden console window.
+        /// Listen for this event to get the output from the hidden console window as it it received.
         /// </summary>
         public static event Action<string> CommandLineOutputReceived;
         
         /// <summary>
-        /// Listen for this event to get the output from the hidden console window.
+        /// Listen for this event to get the full output from the hidden console window.
         /// </summary>
         public static event Action<string> CommandLineOutputComplete;
 
--- SdkManager.UI/ViewModels/Windows/ConsoleOutputWindowViewModel.cs
+++ SdkManager.UI/ViewModels/Windows/ConsoleOutputWindowViewModel.cs
@@ -5,16 +5,16 @@ using System.Windows.Input;
 namespace SdkManager.UI
 {
     /// <summary>
-    /// A window to diplay when updating, intalling, uninstalling packages.
-    /// <para>Will return true if user accpets, or false if user cancles.</para>
-    /// <para>Use Window.ShowDialog()</para>
     /// </summary>
     public class ConsoleOutputWindowViewModel : BaseViewModel
     {
-        private Window _win {get;set;}
 
+        private Window _win {get;set;}
         private SdkManagerBatViewModel sdkManagerbatVM;
-        public SdkManagerBatViewModel SdkManagerBatVM { get => sdkManagerbatVM;
+
+        public SdkManagerBatViewModel SdkManagerBatVM
+        {
+            get => sdkManagerbatVM;
             set
             {
                 if(sdkManagerbatVM != value)
